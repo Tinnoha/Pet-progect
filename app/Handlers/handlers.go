9e0e15b86d291b7674c61d6da041a5252b48f408
Tinnoha <tspitsyn@sfedu.ru>
2025-08-05 20:11:53 +0300

    Post - запрос передает значения с JSON ы Postgres

--- app/Handlers/handlers.go
+++ app/Handlers/handlers.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+
 	"log"
 	"net/http"
 	"strconv"
@@ -34,11 +36,7 @@ func handlGetList(writer http.ResponseWriter, request *http.Request) {
 		}
 		for rezult.Next() {
 			var user_id int
-			var full_name string
-			var username string
-			var email string
-			var password_hash string
-			var role string
+			var full_name, username, email, password_hash, role string
 			var created_at time.Time
 			err := rezult.Scan(&user_id, &full_name, &username, &email, &password_hash, &role, &created_at)
 			if err != nil {
@@ -51,6 +49,55 @@ func handlGetList(writer http.ResponseWriter, request *http.Request) {
 
 	} else if request.Method == http.MethodPost {
 		fmt.Fprint(writer, "Give your data")
+
+		petya := models.User{}
+
+		err := json.NewDecoder(request.Body).Decode(&petya)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		DB, err := sql.Open("postgres", models.ConnStr)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = DB.Ping()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Print(petya)
+
+		_, err = DB.Exec(
+			"INSERT INTO polzovately (id,first_name,middle_name,last_name,PasswordHash, email, balance) values ($1,$2, $3, $4, $5, $6, $7)",
+			petya.Id,
+			petya.First_name,
+			petya.Middle_name,
+			petya.Last_name,
+			petya.PasswordHash,
+			petya.Email,
+			petya.Balance,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		DB.Close()
+
+		/*
+			create table polzovately ( Таблица юзеров
+				id serial,
+				first_name varchar[40],
+				middle_name varchar[40],
+				last_name varchar[40],
+				PasswordHash varchar,
+				email varchar [100],
+				balance bigint default 0
+			);
+		*/
+
 	}
 }
 
--- app/models/model.go
+++ app/models/model.go
@@ -1,3 +1,23 @@
 package models
 
 const ConnStr string = "host=localhost port=5432 user=postgres password=1234 dbname=postgres sslmode=disable"
+
+type User struct {
+	Id           int    `json:"id"`
+	First_name   string `json:"first_name"`
+	Middle_name  string `json:"middle_name"`
+	Last_name    string `json:"last_name"`
+	PasswordHash string `json:"passwordHash"`
+	Email        string `json:"email"`
+	Balance      int64  `json:"balance"`
+}
+
+// {
+//     "id": 1,
+//     "first_name": "Иван",
+//     "middle_name": "Иванович",
+//     "last_name": "Иванов",
+//     "PasswordHash": "abc123",
+//     "email": "ivan@example.com",
+//     "balance": 1000
+// }
